8099a2466c6c3ecb62dd0131e9d41d29c5e85f96
Freddy92i <freddypeltier21@outlook.fr>
2022-05-23 21:02:38 +0200

    fix admin part

--- admin/home.php
+++ admin/home.php
@@ -3,7 +3,7 @@
   session_start();
   // Vérifiez si l'utilisateur est connecté, sinon redirigez-le vers la page de connexion
   if(!isset($_SESSION["username"])){
-    header("location: log.php");
+    header("location: ../log.php");
     exit(); 
   }
 ?>
