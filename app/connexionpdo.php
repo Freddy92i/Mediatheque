ae14877750f8c514742c1d1cd3c8a3cb7be4cd43
Freddy Peltier <84007024+Freddy92i@users.noreply.github.com>
2022-05-29 19:41:26 +0200

    Update connexionpdo.php

--- app/connexionpdo.php
+++ app/connexionpdo.php
@@ -3,10 +3,10 @@
     {
         // On se connecte à la base de donnée inscription avec le compte
         // serveur
-        // $bdd= new PDO('mysql:host=localhost;dbname=mediatheque','pfreddy','aden_pfreddy$2022',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+         $bdd= new PDO('mysql:host=localhost;dbname=mediatheque','pfreddy','aden_pfreddy$2022',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
         
         // Local
-        $bdd= new PDO('mysql:host=localhost;dbname=mediatheque','root','root',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        // $bdd= new PDO('mysql:host=localhost;dbname=mediatheque','root','root',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 
     }
     catch(Exception $e)
