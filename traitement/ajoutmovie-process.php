bc6aa4ccfc3262e5152c2dd7eb628d5a9ccbdda0
Freddy92i <freddypeltier21@outlook.fr>
2022-05-30 17:47:47 +0200

    essai ajoutfilm

--- traitement/ajoutmovie-process.php
+++ traitement/ajoutmovie-process.php
@@ -12,29 +12,20 @@ if(empty($_COOKIE['id'])) {
     $realisateur = $_POST['realisateur'];
     $categorie = $_POST['categorie'];
     $image = $_POST['image'];
+    $imagealt = $_POST['imagealt'];
 
-    if (empty($nom) || empty($duree) || empty($resume) || empty($realisateur) || empty($categorie) || empty($image)) { // Gestion d'erreur si un des champs est vide
-        // Création de la session message pour y afficher le message de confirmation
+    if (empty($nom) || empty($duree) || empty($resume) || empty($realisateur) || empty($categorie) || empty($image) || empty($imagealt)) {
+        // Création de la session message pour y afficher le message d'erreur
         $_SESSION['message'] = 'Un des champs est vide';
         header('location: ../ajoutmovie.php');
-    } elseif(strlen($nom) > 40 || strlen($duree) > 3 || strlen($resume) > 500 || strlen($realisateur) > 40 || strlen($image) > 500) {
-        $_SESSION['message'] = 'Problème d\'insertion dans la base de donnée';
-        header('location: ../ajoutmovie.php');
-    }
-    else { // Si tous les champs sont remplis, on execute la requete SQL
-        $req = $bdd->prepare('INSERT INTO film VALUES(:id,:nom,:duree,:resume,:realisateur,:categorie,:image)');
-        $req->execute(array(
-                'id' => NULL,
-                'nom' => $nom,
-                'duree' => $duree,
-                'resume' => $resume,
-                'realisateur' => $realisateur,
-                'categorie' => $categorie,
-                'image' => $image,)
-        );
+    } else {
+        $req = $bdd->prepare("UPDATE film SET nom = ?, duree = ?, resume = ?, realisateur = ?, RefCat = ?, img = ?, imagealt = ? ");
+        $req->execute(array($nom, $duree, $resume, $realisateur, $categorie, $image, $imagealt));
+
         // Création de la session message pour y afficher le message de confirmation
         $_SESSION['message'] = 'Ajout du film effectué avec succès';
         header('location: ../mediatheque.php');
+
     }
 }
 ?>
