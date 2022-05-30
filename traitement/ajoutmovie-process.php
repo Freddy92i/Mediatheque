10e9f02f0082b9dd4123f9a6e1c336a31cbfdd11
Freddy92i <freddypeltier21@outlook.fr>
2022-05-30 16:33:47 +0200

    essai ajoutfilm

--- traitement/ajoutmovie-process.php
+++ traitement/ajoutmovie-process.php
@@ -13,16 +13,15 @@ if(empty($_COOKIE['id'])) {
     $realisateur = $_POST['realisateur'];
     $categorie = $_POST['categorie'];
     $image = $_POST['image'];
-    $imagealt = $_POST['imagealt'];
 
 
-    if (empty($_POST['nom']) OR empty($_POST['duree']) OR empty($_POST['resume']) OR empty($_POST['realisateur']) OR empty($_POST['categorie']) OR empty($_POST['image']) OR empty($_POST['imagealt'])) {  // Gestion d'erreur
+    if (empty($_POST['nom']) OR empty($_POST['duree']) OR empty($_POST['resume']) OR empty($_POST['realisateur']) OR empty($_POST['categorie']) OR empty($_POST['image'])) {  // Gestion d'erreur
         // Création de la session message pour y afficher le message d'erreur
         $_SESSION['message'] = 'Erreur de champs';
         header('location: ../mediatheque.php');
     } else { // S'il n'y a pas d'erreur, on execute la requête SQL pour insérer les données dans la table information
-        $req = $bdd->prepare('INSERT INTO `film` (`id`,`nom`,`duree`,`resume`,`realisateur`,`categorie`,`image`,`imagealt`) VALUES(:id,:nom,:duree,:_resume,:realisateur,:categorie,:_image,:imagealt)');
-        $req->execute(array('id' => NULL, 'mail' => $nom, 'duree' => $duree, 'resume' => $resume, 'realisateur' => $realisateur, 'categorie' => $categorie, 'image' => $image, 'imagealt' => $imagealt,));
+        $req = $bdd->prepare('INSERT INTO `film` (`id`,`nom`,`duree`,`resume`,`realisateur`,`categorie`,`image`) VALUES(:id,:nom,:duree,:_resume,:realisateur,:categorie,:_image,)');
+        $req->execute(array('id' => NULL, 'mail' => $nom, 'duree' => $duree, 'resume' => $resume, 'realisateur' => $realisateur, 'categorie' => $categorie, 'image' => $image,));
         // Création de la session message pour y afficher le message de confirmation
         $_SESSION['message'] = 'Inscription effectue avec succes';
         header('location:../log.php');
