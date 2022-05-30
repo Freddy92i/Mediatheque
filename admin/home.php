089436aa3a19e88d96592c1b926eb619e969d712
Freddy92i <freddypeltier21@outlook.fr>
2022-05-30 14:30:01 +0200

    essai admin part

--- admin/home.php
+++ admin/home.php
@@ -10,10 +10,10 @@
     <div class="sucess">
     <h1>Bienvenue <?php echo $_SESSION['username']; ?>!</h1>
     <p>C'est votre espace admin.</p>
-    <a href="add_user.php">Add user</a> | 
+    <a href="add_user.php">ajouter un utilisateurs</a> | 
     <a href="#">Update user</a> | 
     <a href="#">Delete user</a> | 
-    <a href="../logout.php">Déconnexion</a>
+    <a href="../traitement/traitementdeconnexion.php">Déconnexion</a>
     </ul>
     </div>
   </body>
