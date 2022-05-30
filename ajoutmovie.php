6319707a53c1d0e5813fb4945574845e755bb4f9
Freddy92i <freddypeltier21@outlook.fr>
2022-05-30 16:09:48 +0200

    test ajoutfilm

--- ajoutmovie.php
+++ ajoutmovie.php
@@ -85,7 +85,7 @@ if(isset($_SESSION['message'])) { // Si il y a un quelconque message dans le $_S
                 </div>
                 <div class="form-group">
                     <label>Image_alternative</label>
-                    <input type="text" name="img_alt" class="form-control" placeholder="Entrer l'url de votre image alternative">
+                    <input type="text" name="imagealt" class="form-control" placeholder="Entrer l'url de votre image alternative">
                 </div>
             </div>
         </div>
