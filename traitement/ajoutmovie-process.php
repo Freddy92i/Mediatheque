c1b7796b7fc18548535b16c2068a9cce9d7bb580
Freddy92i <freddypeltier21@outlook.fr>
2022-05-30 17:33:34 +0200

    essai ajoutfilm

--- traitement/ajoutmovie-process.php
+++ traitement/ajoutmovie-process.php
@@ -13,26 +13,28 @@ if(empty($_COOKIE['id'])) {
     $categorie = $_POST['categorie'];
     $image = $_POST['image'];
 
-    if (empty($nom) || empty($duree) || empty($resume) || empty($realisateur) || empty($categorie) || empty($image)) {
-        // Création de la session message pour y afficher le message d'erreur
+    if (empty($nom) || empty($duree) || empty($resume) || empty($realisateur) || empty($categorie) || empty($image)) { // Gestion d'erreur si un des champs est vide
+        // Création de la session message pour y afficher le message de confirmation
         $_SESSION['message'] = 'Un des champs est vide';
-        header('location: ../ajoutmovie.php');
-    } else {
+        header('location: ../ajoutMovie.php');
+    } elseif(strlen($nom) > 40 || strlen($duree) > 3 || strlen($resume) > 500 || strlen($realisateur) > 40 || strlen($image) > 500) {
+        $_SESSION['message'] = 'Problème d\'insertion dans la base de donnée';
+        header('location: ../ajoutMovie.php');
+    }
+    else { // Si tous les champs sont remplis, on execute la requete SQL
         $req = $bdd->prepare('INSERT INTO film VALUES(:id,:nom,:duree,:resume,:realisateur,:categorie,:image)');
         $req->execute(array(
-            'id' => NULL,
-            'nom' => $nom,
-            'duree' => $duree,
-            'resume' => $resume,
-            'realisateur' => $realisateur,
-            'categorie' => $categorie,
-            'image' => $image,)
-    );
-
+                'id' => NULL,
+                'nom' => $nom,
+                'duree' => $duree,
+                'resume' => $resume,
+                'realisateur' => $realisateur,
+                'categorie' => $categorie,
+                'image' => $image,)
+        );
         // Création de la session message pour y afficher le message de confirmation
         $_SESSION['message'] = 'Ajout du film effectué avec succès';
-        header('location: ../mediatheque.php');
-
+        header('location: ../mediateque.php');
     }
 }
 ?>
