2086a1fe0851221002ad997c258f51b82e4cdbde
Freddy92i <freddypeltier21@outlook.fr>
2022-06-01 18:00:55 +0200

    try add user.php

--- admin/home.php
+++ admin/home.php
@@ -9,9 +9,9 @@
         <h1 class="admin-h1">Bienvenue <?php echo $_SESSION['username']; ?>!</h1>
         <p class="admin-text" >C'est votre espace admin.</p>
         <ul class="admin-list" >
-          <a class="admin-fonctions" href="add_user.php">ajouter un utilisateurs</a>&nbsp;&nbsp; | &nbsp;&nbsp;
-          <a class="admin-fonctions" href="update_user.php">Modifier un utilisateur</a>&nbsp;&nbsp; | &nbsp;&nbsp;
-          <a class="admin-fonctions" href="delete_user.php">supprimer un utilisateur</a>&nbsp;&nbsp; | &nbsp;&nbsp; &nbsp;&nbsp;
+          <a class="admin-fonctions" href="adduser.php">ajouter un utilisateurs</a>&nbsp;&nbsp; | &nbsp;&nbsp;
+          <a class="admin-fonctions" href="updateuser.php">Modifier un utilisateur</a>&nbsp;&nbsp; | &nbsp;&nbsp;
+          <a class="admin-fonctions" href="deleteuser.php">supprimer un utilisateur</a>&nbsp;&nbsp; | &nbsp;&nbsp; &nbsp;&nbsp;
           <a class="admin-fonctions" href="../traitement/traitementdeconnexion.php">Déconnexion</a>
         </ul>
       </div>
--- traitement/adduser-process.php
+++ traitement/adduser-process.php
@@ -17,7 +17,7 @@
     if (empty($_POST['mail']) OR empty($_POST['mdp']) OR empty($_POST['prenom']) OR empty($_POST['nom']) OR strlen($mdp) < 6) {  // Gestion d'erreur
         // Création de la session message pour y afficher le message d'erreur
         $_SESSION['message'] = 'Erreur de champs';
-        header('location: ../admin/add_user.php');
+        header('location: ../admin/adduser.php');
     } else { // S'il n'y a pas d'erreur, on execute la requête SQL pour insérer les données dans la table information
         $role = 'guest';
         $mdp = password_hash($mdp, $algo);
@@ -25,6 +25,6 @@
         $req->execute(array('id' => NULL, 'mail' => $mail, '_role' => $role, 'mdp' => $mdp, 'prenom' => $prenom, 'nom' => $nom));
         // Création de la session message pour y afficher le message de confirmation
         $_SESSION['message'] = 'Inscription effectue avec succes';
-        header('location:../admin/add_user.php');
+        header('location:../admin/adduser.php');
     }
 ?>
