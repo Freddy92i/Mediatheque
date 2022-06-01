c6cc0be14ae53c65f796e25425beb30164ae1efe
Freddy92i <freddypeltier21@outlook.fr>
2022-06-01 17:26:24 +0200

    try home.php

--- admin/home.php
+++ admin/home.php
@@ -11,8 +11,8 @@
         <ul class="admin-list" >
           <a class="admin-fonctions" href="add_user.php">ajouter un utilisateurs</a>&nbsp;&nbsp; | &nbsp;&nbsp;
           <a class="admin-fonctions" href="#">Update user</a>&nbsp;&nbsp; | &nbsp;&nbsp;
-          <a class="admin-fonctions" href="#">Delete user</a>&nbsp;&nbsp; | &nbsp;&nbsp;
-          <a class="admin-fonctions" href="../traitement/traitementdeconnexion.php">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Déconnexion</a>
+          <a class="admin-fonctions" href="#">Delete user</a>&nbsp;&nbsp; | &nbsp;&nbsp; &nbsp;&nbsp;
+          <a class="admin-fonctions" href="../traitement/traitementdeconnexion.php">Déconnexion</a>
         </ul>
       </div>
   </body>
