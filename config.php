47edcfa31d73753b104e4ecd59072fab68f916a2
Freddy92i <freddypeltier21@outlook.fr>
2023-05-04 13:14:52 +0200

    divers fix + update for server

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 // Informations de connexion à la base de données
-define('DB_HOST', 'localhost');
+define('DB_HOST', '');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'root');
 define('DB_NAME', 'mediatheque_numerique');
@@ -8,7 +8,7 @@ define('DB_NAME', 'mediatheque_numerique');
 // Chemin à la racine du projet
 define('ROOT_PATH', dirname(__FILE__));
 
-define('SITE_URL', 'http://localhost/mediatheque_numerique');
+define('SITE_URL', '/mediatheque_numerique');
 
 // Nom du projet
 define('PROJECT_NAME', 'Mediatheque Numérique');
