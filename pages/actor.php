2b1727b1026254caae2866f189945f4258c2f231
Freddy92i <freddypeltier21@outlook.fr>
2023-05-08 17:38:05 +0200

    fix actor

--- pages/actor.php
+++ pages/actor.php
@@ -1,7 +1,14 @@
 <?php
 include '../config.php';
-include "../interface/navbar.php";
+?>
+<html>
+    <head>
+        <link href="<?= SITE_URL ?>/style/style.css" rel="stylesheet">
+    </head>
+<body style="background-color: #131c27;">
 
+<?php
+include '../interface/navbar.php';
 
 // Clé API TMDB
 $api_key = "819b3b9f2b6ec0428a3888d8c512ba08";
